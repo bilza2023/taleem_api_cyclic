29d609a2bec9589be603f1733b2b1a9c16ea78da
unknown <bilza32611@gmail.com>
2024-02-11 09:18:57 +0500

    port changed to 3000 , on / resp is 200 now

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const mongoose = require('mongoose');
 // const auth = require('./auth/auth.js');
 ////////////////////////////////////////////////
 const cookieParser = require('cookie-parser');
-const PORT = process.env.PORT || 5000;
+const PORT = 3000;
 ////////////////////////////////////////////////////
 // debugger;
 const app = express()
@@ -21,7 +21,7 @@ app.use(express.urlencoded({ extended: true }));
 // app.use("/auth",auth);
 ///////////////////////////Routes////////////////////////
 app.get('/', async (req, res) =>{
-    res.status(500).json({success :true ,  message : "Welcome to Taleem Cyclic API"});
+    res.status(200).json({success :true ,  message : "Welcome to Taleem Cyclic API"});
 });
 ///////////////////////////////////////////////////////////////////////
 mongoose.connect( process.env.DB_URL , { useNewUrlParser: true});
